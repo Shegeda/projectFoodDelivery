181fe05f068144d761176a1956c7d3bfd05d3be8
Shegeda <shegedasasha@gmail.com>
2021-05-05 16:33:06 +0300

    v1.1 add timer

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 window.addEventListener('DOMContentLoaded', () => {
 
+    //Tabs
+
     const tabs = document.querySelectorAll('.tabheader__item'),
-          tabsContent = document.querySelectorAll('.tabcontent'),
-          tabsParent = document.querySelector('.tabheader__items');
+        tabsContent = document.querySelectorAll('.tabcontent'),
+        tabsParent = document.querySelector('.tabheader__items');
 
     function hideTabContent() {
         tabsContent.forEach(item => {
@@ -17,14 +19,14 @@ window.addEventListener('DOMContentLoaded', () => {
         });
     }
 
-    function showTabContent(i = 0) {    
+    function showTabContent(i = 0) {
         //  tabsContent[i].style.display = 'block';
         tabsContent[i].classList.add('show', 'fade');
         tabsContent[i].classList.remove('hide');
-        tabs[i].classList.add('tabheader__item_active'); 
+        tabs[i].classList.add('tabheader__item_active');
     }
 
-    hideTabContent();   
+    hideTabContent();
     showTabContent();
 
     tabsParent.addEventListener('click', (event) => {
@@ -39,6 +41,63 @@ window.addEventListener('DOMContentLoaded', () => {
             });
         }
     });
-});
 
-  
+    // Timer
+
+    const deadLine = '2021-05-06';
+
+    // Расчет
+    function getTimeRemaining(endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+            days = Math.floor(t / (1000 * 60 * 60 * 24)),
+            hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+            minutes = Math.floor(t / (1000 * 60) % 60),
+            seconds = Math.floor((t / 1000) % 60);
+
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        }
+    }
+
+    // Для добавления нулей "00:00:00"
+    function getZero(num) {
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
+
+    // Установка таймера на страницу
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector),
+            days = timer.querySelector('#days'),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+        // Обновляет таймер каждую секунду
+        // Вызов updateClock "чтоб таймер не мигал"
+        updateClock();
+
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
+
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+            // Остановка таймера
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+
+    }
+
+    setClock('.timer', deadLine);
+});
